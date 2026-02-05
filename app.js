bdd48916d8b15524980cd5cd36aafbccc1ac19a4
Khushi2065 <khushimodh1@gmail.com>
2026-02-05 13:22:47 +0530

    My Portfolio

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-const express = require('express');
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const app = express();
-app.get('/', (req, res) => {
-    res.send('Hello, this is a code by Ramanpreet to test pipeline and this is the first version');
-  });
-  const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`API server listening on port ${port}`);
+
+// Fix __dirname in ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const _dirname = path.dirname(__filename);
+
+// Serve static files from current folder
+app.use(express.static(_dirname));
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`portfolio running at http://localhost:${PORT}`);
 });
