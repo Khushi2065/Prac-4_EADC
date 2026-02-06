e06437204368846b588e2913423a04c8f7567d6f
Khushi2065 <khushimodh1@gmail.com>
2026-02-06 08:16:19 +0530

    Portfolio added

--- app.js
+++ app.js
@@ -3,15 +3,10 @@ const path = require('path');
 const port= process.env.port || 3000 
 const app = express()
 
-// Fix __dirname in ES Modules
 app.use(express.static(path.join(__dirname+'/public')))
-// const __filename = fileURLToPath(import.meta.url);
 const _dirname = path.dirname(__filename);
-
 // Serve static files from current folder
 app.use(express.static(_dirname));
-
-  
 app.listen(port, () => {
   console.log(`API server listening on port ${port}`);
 });
