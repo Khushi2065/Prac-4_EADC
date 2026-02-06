f081b1ac1ab07a76151416a0780490b38aa9300e
Khushi2065 <khushimodh1@gmail.com>
2026-02-06 08:25:17 +0530

    changes

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const express = require('express'); 
 const path = require('path'); 
-const port= process.env.port || 3000 
-const app = express()
-
-app.use(express.static(path.join(__dirname+'/public')))
-const _dirname = path.dirname(__filename);
-// Serve static files from current folder
-app.use(express.static(_dirname));
-app.listen(port, () => {
-  console.log(`API server listening on port ${port}`);
-});
+const app = express(); 
+const port = process.env.PORT || 3000; 
+app.use(express.static(path.join(__dirname+'/public'))) 
+// Serve static files from current folder 
+app.use(express.static(__dirname)); 
+app.listen(port, () => { 
+console.log(`API server listening on port ${port}`); 
+}); 
