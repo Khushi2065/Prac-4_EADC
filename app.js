50f01828513e437198acdeec41ac3591fb723363
Khushi2065 <khushimodh1@gmail.com>
2026-02-06 08:29:41 +0530

    changes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const port= process.env.port || 3000;
 app.use(express.static(path.join(__dirname+'/public')))
-const _dirname = path.dirname(__filename);
+// const _dirname = path.dirname(__filename);
 // Serve static files from current folder
 app.use(express.static(_dirname));
 app.listen(port, () => {
